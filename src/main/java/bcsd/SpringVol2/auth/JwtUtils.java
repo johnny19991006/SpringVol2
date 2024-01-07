9d4341aa3a5858900227c21dafe24cdfaa1c49e9
kwoo28 <148550522+kwoo28@users.noreply.github.com>
2024-01-08 00:38:12 +0900

    jwt 인증 방식

---
+++ src/main/java/bcsd/SpringVol2/auth/JwtUtils.java
@@ -0,0 +1,46 @@
+package bcsd.SpringVol2.auth;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import jakarta.xml.bind.DatatypeConverter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.spec.SecretKeySpec;
+import java.security.Key;
+import java.util.Date;
+
+@Component
+public class JwtUtils {
+
+    @Value("${jwt.secret}")
+    private String SECRET_KEY;
+
+    public String createToken(String subject, long expTime){
+        if(expTime<=0){
+            throw new RuntimeException("만료 시간 0초 미만");
+        }
+
+        SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256;
+
+        byte[] secretKeyBytes = DatatypeConverter.parseBase64Binary(SECRET_KEY);
+        Key signingKey = new SecretKeySpec(secretKeyBytes, signatureAlgorithm.getJcaName());
+
+        return Jwts.builder()
+                .setSubject(subject)
+                .signWith(signingKey, signatureAlgorithm)
+                .setExpiration(new Date(System.currentTimeMillis() + expTime))
+                .compact();
+    }
+
+    public boolean isValidToken(String token){
+        Claims claims = Jwts.parserBuilder()
+                .setSigningKey(DatatypeConverter.parseBase64Binary(SECRET_KEY))
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+
+        return claims!=null;
+    }
+}
