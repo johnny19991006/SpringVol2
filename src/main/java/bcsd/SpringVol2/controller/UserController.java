c197d7b35d253301860e28f163d777c6ed59933f
kwoo28 <148550522+kwoo28@users.noreply.github.com>
2024-01-08 16:27:17 +0900

    Feat: 구현 UserController 로그인 기능
    로그인할때 토큰을 헤더에 담아서 발급
    토큰 만료시간 2분으로 제한

--- src/main/java/bcsd/SpringVol2/controller/UserController.java
+++ src/main/java/bcsd/SpringVol2/controller/UserController.java
@@ -1,9 +1,14 @@
 package bcsd.SpringVol2.controller;
 
+import bcsd.SpringVol2.auth.JwtUtils;
+import bcsd.SpringVol2.domain.dto.LoginRequest;
 import bcsd.SpringVol2.domain.entity.User;
 import bcsd.SpringVol2.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,6 +20,7 @@ import java.util.List;
 public class UserController {
 
     private final UserService userService;
+    private final JwtUtils jwtUtils;
 
     @PostMapping
     public void insertUser(@RequestBody User user){
@@ -49,5 +55,20 @@ public class UserController {
     @DeleteMapping("/{id}")
     public void deleteUser(@PathVariable(name = "id") Long id){
         userService.deleteUser(id);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity loginUser(@RequestBody LoginRequest loginRequest){
+        User user = userService.login(loginRequest);
+
+        HttpHeaders headers = new HttpHeaders();
+
+        if(user!=null){
+            String token = jwtUtils.createToken(user.getUserid(),2*1000*60);
+            headers.set("token", token);
+            return new ResponseEntity<>(headers, HttpStatus.OK);
+        } else{
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        }
     }
 }
